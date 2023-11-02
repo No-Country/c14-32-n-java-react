64231f10e6e588da207d6b725dbb1cc5baa66886
Juan Oscco <juan.osccomori@gmail.com>
2023-11-02 14:08:19 -0500

    input password

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -61,7 +61,7 @@ export default function Home() {
             </p>
           )}
           <label className="block text-sm font-medium leading-6 text-gray-900">
-            Email address
+            Password
           </label>
           <input
             type="password"
